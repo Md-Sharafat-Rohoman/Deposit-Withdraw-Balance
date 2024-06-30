137002fd8d03d3d0564a4d46e0f5efd3db880d8d
Md Sharafat Rohoaman <mdsharafatrohomanzafourllah@gmail.com>
2024-06-30 21:45:27 +0600

    last commit

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,1 +1,24 @@
-console.log('MY NAME IS SHARAFAT ROHOMAN')
+document.getElementById('btn-withdraw').addEventListener('click',function(){
+    const withDrawField = document.getElementById('withdraw-field');
+    const newWithDrawTotalString = withDrawField.value;
+    const newWithDrawTotal = parseFloat(newWithDrawTotalString);
+
+    const withDrawTotalElement = document.getElementById('withdraw-total');
+    const previouwWithDrawTotalString = withDrawTotalElement.innerText;
+    const previouwWithDrawTotal = parseFloat(previouwWithDrawTotalString);
+    const currentWithDrawTotal = newWithDrawTotal + previouwWithDrawTotal;
+    // console.log(currentWithDrawTotal);
+    withDrawTotalElement.innerText = currentWithDrawTotal;
+
+    // total balace
+    const balanceTotalElement = document.getElementById('balace-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString); 
+
+    const currentBalanceTotal = previousBalanceTotal - newWithDrawTotal;
+    balanceTotalElement.innerText = currentBalanceTotal; 
+
+
+
+    withDrawField.value = ''
+})
